15846ea4bf0bb19b37ee9bfe5b39ed61cc939839
Paul Broadhead <pjbroad@twinmoons.org.uk>
2018-04-08 22:09:25 +0100

    Bump client release version to 1.9.5

--- elc_private.h
+++ elc_private.h
@@ -5,19 +5,19 @@
 #define ELC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.9.3.0"
+#define VER_STRING	"1.9.5.0"
 #define VER_MAJOR	1
 #define VER_MINOR	9
-#define VER_RELEASE	3
+#define VER_RELEASE	5
 #define VER_BUILD	0
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.9.3"
+#define FILE_VERSION	"1.9.5"
 #define FILE_DESCRIPTION	"Other-Life Client"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Other-Life Client"
-#define PRODUCT_VERSION	"1.9.3"
+#define PRODUCT_VERSION	"1.9.5"
 
 #endif /*ELC_PRIVATE_H*/
